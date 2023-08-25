961311da1d585cafa820f44c7d69a5d4c8a4463e
Goooler <wangzongler@gmail.com>
2023-08-26 00:53:22 +0800

    Migrate Maven to Gradle

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    `java-library`
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("junit:junit:4.13.2")
+}
+
+group = "com.jakewharton.android.repackaged"
+version = "HEAD-SNAPSHOT"
+description = "platform/dalvik dx"
+
+sourceSets {
+    getByName("main").java.srcDir("platform_dalvik/dx/src")
+    getByName("test").java.srcDir("platform_dalvik/dx/junit-tests")
+}
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+tasks.javadoc {
+    (options as CoreJavadocOptions).addStringOption("Xdoclint:none", "-quiet")
+}
